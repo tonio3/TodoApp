3df71ee85de331b4cdd305c18f15ec28970f6539
Jiří Vašák <ji.vasak@live.com>
2022-07-01 13:35:28 +0200

    zmena api

--- api/src/Controllers/TodoController.cs
+++ api/src/Controllers/TodoController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Todolist.Controllers {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class TodoController : ControllerBase {
 
