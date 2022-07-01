4f8fba8f86a2780fa0fc5552bcff7517f29f4750
Jiří Vašák <ji.vasak@live.com>
2022-07-01 11:53:38 +0200

    zmena programu

--- api/src/Program.cs
+++ api/src/Program.cs
@@ -2,6 +2,16 @@ using Microsoft.EntityFrameworkCore;
 using Todolist;
 
 var builder = WebApplication.CreateBuilder();
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+                      });
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -12,7 +22,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseDeveloperExceptionPage();
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
-app.UseAuthorization();
 app.MapControllers();
 app.Run();
