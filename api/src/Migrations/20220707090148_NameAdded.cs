b5ee7b64aa534697ef3089525b443b43222ec87e
Antonio Flores <floresak@seznam.cz>
2022-07-07 11:22:22 +0200

    migrace

---
+++ api/src/Migrations/20220707090148_NameAdded.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todolist.Migrations
+{
+    public partial class NameAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "User",
+                table: "Items",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "User",
+                table: "Items");
+        }
+    }
+}
